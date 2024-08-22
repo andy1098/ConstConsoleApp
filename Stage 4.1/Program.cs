8620bc518058ea7fe34ed8b8d033d4b5316458db
Андрей Викулов <7432721@gmail.com>
2024-08-22 22:25:35 +0300

    последнее задание урока

--- Stage 4.1/Program.cs
+++ Stage 4.1/Program.cs
@@ -4,26 +4,35 @@
     {
         Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
         var color = Console.ReadLine();
-        if (color == "red")
+        switch (color)
         {
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.ForegroundColor = ConsoleColor.Black;
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
 
-            Console.WriteLine("Your color is red!");
-        }
-        else if (color == "green")
-        {
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is red!");
+                break;
 
-            Console.WriteLine("Your color is green!");
-        }
-        else
-        {
-            Console.BackgroundColor = ConsoleColor.Cyan;
-            Console.ForegroundColor = ConsoleColor.Black;
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
 
-            Console.WriteLine("Your color is cyan!");
+                Console.WriteLine("Your color is green!");
+                break;
+
+            case "cyan":
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("Your color is cyan!");
+                break;
+
+            default:
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                Console.WriteLine("Your color from Hell!");
+                break;
         }
     }
 }
