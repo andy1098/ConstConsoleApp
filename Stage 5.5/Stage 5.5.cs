de1b68a2b4ce21defe4cb423b1343c4fce2b7402
Андрей Викулов <7432721@gmail.com>
2024-09-02 00:11:02 +0300

    5.6 начало

--- Stage 5.5/Stage 5.5.cs
+++ Stage 5.5/Stage 5.5.cs
@@ -3,23 +3,36 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Напишите что-то");
-        var str = Console.ReadLine();
+        // var str = Console.ReadLine();
         Console.WriteLine("Укажите глубину эха");
-        var deep = int.Parse(Console.ReadLine());
-        
-        Echo(str, deep);
+        // var deep = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(PowerUp(0, 9));
+        // Echo(str, deep);
         Console.ReadKey();
     }
 
     static void Echo(string saidworld, int deep)
     {
         var modif = saidworld;
-        if (modif.Length > 2)
+        if (modif.Length > 2) 
             modif = modif.Remove(0, 2);
+        Console.BackgroundColor = (ConsoleColor)deep;
         Console.WriteLine("... " + modif);
         if (deep > 1)
         {
             Echo(modif, deep - 1);
+        }
+    }
+    static int PowerUp(int N, byte pow)
+    {
+        if (N == 0 & pow == 0) return 0;
+        if (N == 0 | pow == 0) return 1;
+        else
+        {
+            if (pow == 1) return N;
+            else
+                return N = N * PowerUp(N, --pow);
         }
     }
 }
---
+++ Stage 5.6/Stage 5.6.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    // (string name, string surname, string login, int login_count, int age, bool has_pet, string[] colors) anketa;
+
+    private static void Main(string[] args)
+    {
+        
+    }
+
+    static object EnterData()
+    {
+        Console.Write("Введите имя:");
+        var name = Console.ReadLine();
+        Console.Write("Введите фамилию:");
+        var surname = Console.ReadLine();
+        Console.Write("Введите возраст пользователя: ");
+        var age = int.Parse(Console.ReadLine()); // нужна проверка
+
+        Console.Write("У Вас есть домашний питомец? ");
+        bool has_pet = Console.ReadLine() == "да" ? true : false;
+
+        Console.Write("Укажите количество домашних питомцев - ");
+        var petn = Console.ReadLine();
+
+        int petnum = 0;
+        do
+            if (!int.TryParse(petn, out petnum))
+            {
+                Console.WriteLine("Некорректное значение, введите целое положительное число - ");
+                petn = Console.ReadLine();
+            }
+        while (petnum == 0);
+
+        string[] colors = new string[3];
+        Console.WriteLine("Введите три любимых цвета пользователя");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("Введите цвет: ");
+            var colors[i] = Console.ReadLine();
+        }
+
+        return (name, surname, login, age, has_pet, colors);
+    
+        
+    }
+}
