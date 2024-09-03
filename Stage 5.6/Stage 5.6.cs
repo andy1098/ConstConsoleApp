978928fee88600039fa909f9ea5a138c30ede5d4
Андрей Викулов <7432721@gmail.com>
2024-09-03 08:49:48 +0300

    5.6 закончен

--- Stage 5.6/Stage 5.6.cs
+++ Stage 5.6/Stage 5.6.cs
@@ -1,42 +1,27 @@
 ﻿internal class Program
 {
-    
-
+   
     private static void Main(string[] args)
-    
     {
-        (string name, string surname, int age, bool has_pet, int numpet, string[] namespet, int numcolor, string[] colors) Anketa;
-
         string yn = "";
         do
-        {
-            Anketa = EnterData();
-            Console.WriteLine("--------------------------------------");
-            Console.WriteLine("Ваше имя и фамилия: " + Anketa.name + " " + Anketa.surname);
-            if (Anketa.namespet[0] != "")
-            {
-                var i = 1;
-                foreach (string name in Anketa.namespet)
-                {
-                    Console.WriteLine("Имя {0}-го питомца {1}", i, name);
-                    i++;
-                }
-            }
-
+        { 
+            WriteTuple(EnterData());
             Console.WriteLine("--------------------------------------");
             Console.Write("Повторим? y/n: ");
             yn = Console.ReadLine();
 
-            int k = 1;    
-            while (k == 1)
+            bool k = true;    
+            while (k)
             {
                 switch (yn)
                 {
                     case "y":
-                        k = 0;
+                        k = false;
+                        Console.WriteLine("--------------------------------------");
                         break;
                     case "n":
-                        k = 0;
+                        k = false;
                         break;
                     default:
                         Console.Write("Прошу ввести корректный ответ - y/n: ");
@@ -46,30 +31,77 @@
             }
         }
         while (yn != "n");
-        // Console.ReadKey();
     }
 
-    
-    static (string, string, int, bool, int, string[], int, string[]) EnterData()
+    // Вывод данных в консоль
+    static void WriteTuple((string name, string surname, int age, bool has_pet, int numpet, string[] namespet, int numcolor, string[] colors) anketa)
+    {
+        Console.WriteLine("--------------------------------------");
+        Console.WriteLine("Ваше имя и фамилия: " + anketa.name + " " + anketa.surname);
+        Console.WriteLine("Вам {0} полных лет.", anketa.age);
+        if (anketa.has_pet)
+        {
+            var i = 1;
+            foreach (string name in anketa.namespet)
+            {
+                Console.WriteLine("Имя {0}-го питомца: {1};", i, name);
+                i++;
+            }
+        }
+        else
+            Console.WriteLine("У пользователя нет домашних питомцев.");
+
+        if (anketa.colors[0] != "")
+        {
+            var i = 1;
+            foreach (string color in anketa.colors)
+            {
+                Console.WriteLine("Название {0}-го любимого цвета: {1};", i, color);
+                i++;
+            }
+        }
+        else
+            Console.WriteLine("У пользователя нет любимых цветов.");
+    }        
+    static (string, string, int, bool, int, string[], int, string[]) EnterData() // Ввод данных
     {
         string[] colors, petsname;
 
-        Console.Write("Введите имя:");
+        Console.Write("Введите имя: ");
         var name = Console.ReadLine();
-        Console.Write("Введите фамилию:");
+        Console.Write("Введите фамилию: ");
         var surname = Console.ReadLine();
         Console.Write("Введите возраст пользователя: ");
-        var age = ToInt(Console.ReadLine());
+        var age = ToInt(1);
 
         var petnum = 0;
-        Console.Write("У Вас есть домашний питомец? ");
-        bool has_pet = Console.ReadLine() == "да" ? true : false;
-
-        // string[] colors = { "" };
+        Console.Write("У Вас есть домашний питомец (да/нет)? ");
+        
+        //bool has_pet = Console.ReadLine() == "да" ? true : false;
+        
+        bool control = true;
+        bool has_pet = true;
+        while (control)
+        {
+            switch (Console.ReadLine())
+            {
+                case "да":
+                    control = false;
+                    break;
+                case "нет":
+                    control = false;
+                    has_pet = false;
+                    break;
+                default:
+                    Console.Write("Не понял, прошу ввести корректный ответ - да или нет: ");
+                    break;
+            }
+        }
+        
         if (has_pet)
         { 
             Console.Write("Укажите количество домашних питомцев: ");
-            petnum = ToInt(Console.ReadLine());
+            petnum = ToInt(1);
             petsname = GetArray(petnum, "Укажите имя домашнего питомца ");
         }
         else
@@ -78,7 +110,7 @@
         }
 
         Console.Write("Введите количество любимых цветов: ");
-        var numcolors = ToInt(Console.ReadLine());
+        var numcolors = ToInt(0);
         if (numcolors != 0)
         {
             colors = GetArray(numcolors, "Введите любимый цвет #");
@@ -88,23 +120,6 @@
             colors =  new[] { "" };
         }
         return (name, surname, age, has_pet, petnum, petsname, numcolors, colors);
-
-       // if (petsname[0] == "")
-        //{
-          //  if (colors[0] == "")
-           // {
-           //     return (name, surname, age, has_pet, petnum, numcolors);
-           // }
-            //else
-           // {
-            //    return (name, surname, age, has_pet, petnum, numcolors, colors);
-           // }
-       // }
-        //else
-        //{
-         //   return (name, surname, age, has_pet, petnum, petsname, numcolors, colors);
-        //}
-        
     }
     static string[] GetArray(int num, string text) // заполняет массив 
     {
@@ -117,17 +132,30 @@
         }
         return mas;
     }
-    static int ToInt(string petn) // Проверка целого положительного числа
+    static int ToInt(int min) // Проверка целого положительного числа
     {
+        var petn = Console.ReadLine();
+        bool is_int = false;
         int num = -1;
         do
+        {
             if (!int.TryParse(petn, out num))
             {
                 Console.Write("Некорректное значение, введите целое положительное число: ");
                 petn = Console.ReadLine();
             }
-        while (num == -1); 
+            else if ((num = int.Parse(petn)) < min)
+            {
+                Console.Write("Некорректное значение, введите целое положительное число: ");
+                petn = Console.ReadLine();
+            }
+            else
+            {
+                num = int.Parse(petn);
+                is_int = true;
+            }
+        }
+        while (!is_int); 
         return num;
     }
-  
 }
